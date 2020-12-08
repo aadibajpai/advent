7a4bb14dff91809952aa30975b81d4b3a13e4b40
Aadi Bajpai <hello@aadibajpai.com>
2020-12-08 10:50:25 +0530

    day 8 p2

--- day8.py
+++ day8.py
@@ -1,20 +1,32 @@
 with open("input.txt") as f:
     lines = f.read().splitlines()
 
-acc = 0
-visited = [False for _ in range(len(lines))]
 
-i = 0
-while i < len(lines):
-    if visited[i]:
-        break
-    l = lines[i]
-    visited[i] = True
-    if l[:3] == "acc":
-        acc += int(l[4:])
-    elif l[:3] == "jmp":
-        i += int(l[4:])
-        continue
-    i += 1
+def f(lines):
+    acc = 0
+    visited = [False for _ in range(len(lines))]
 
-print(acc)
+    i = 0
+    while i < len(lines):
+        if visited[i]:
+            return None
+        l = lines[i]
+        visited[i] = True
+        if l[:3] == "acc":
+            acc += int(l[4:])
+        elif l[:3] == "jmp":
+            i += int(l[4:])
+            continue
+        i += 1
+    return acc
+
+
+for i in range(len(lines)):
+    ll = lines[:]
+    if ll[i][:3] == "jmp":
+        ll[i] = ll[i].replace("jmp", "nop")
+    else:
+        ll[i] = ll[i].replace("nop", "jmp")
+    acc = f(ll)
+    if acc:
+        print(acc)
