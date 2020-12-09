ee7a1e563bc842f5c6aa2167a8faa2ab790cc675
Aadi Bajpai <hello@aadibajpai.com>
2020-12-09 10:42:22 +0530

    day9 p1 lezgo

--- day9.py
+++ day9.py
@@ -1,3 +1,17 @@
 #!/usr/bin/env python3
 with open("input.txt") as f:
     lines = f.read().splitlines()
+
+pre = [int(lines[i]) for i in range(25)]
+
+lines = lines[25:]
+
+for line in lines:
+    for i in pre:
+        if int(line) - i in pre:
+            pre.pop(0)
+            pre.append(int(line))
+            break
+    else:
+        print(line)
+        break
