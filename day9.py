81a851374ad0bb149eaca0adcdaee0241470b064
Aadi Bajpai <hello@aadibajpai.com>
2020-12-09 10:59:29 +0530

    day9 p2

--- day9.py
+++ day9.py
@@ -2,16 +2,25 @@
 with open("input.txt") as f:
     lines = f.read().splitlines()
 
-pre = [int(lines[i]) for i in range(25)]
+lines = [int(li) for li in lines]
+pre = [lines[i] for i in range(25)]
 
-lines = lines[25:]
+linez = lines[25:]
 
-for line in lines:
+for line in linez:
     for i in pre:
-        if int(line) - i in pre:
+        if line - i in pre:
             pre.pop(0)
-            pre.append(int(line))
+            pre.append(line)
             break
     else:
         print(line)
+        ll = line
         break
+
+for i in range(len(lines)):
+    for j in range(i, len(lines)):
+        window = lines[i:j]
+
+        if sum(window) == ll:
+            print(min(window) + max(window))
